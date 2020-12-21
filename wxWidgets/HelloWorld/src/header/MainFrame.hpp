6f9291853e5388e2b5e4abeb57d5315714f8e2e4
a1-lab <alphamailix@gmail.com>
2020-12-21 17:47:45 +0200

    HelloWorld: add some event handlers
    1) EventHandlers OnAbout, OnExit are added
    2) OnAbout is added dynamically

--- wxWidgets/HelloWorld/src/header/MainFrame.hpp
+++ wxWidgets/HelloWorld/src/header/MainFrame.hpp
@@ -5,4 +5,7 @@ class MainFrame : public wxFrame
 {
 public:
 	explicit MainFrame(const wxString& title);
+	void OnQuit(wxCommandEvent&);
+	void OnAbout(wxCommandEvent&);
+	DECLARE_EVENT_TABLE()
 };
--- wxWidgets/HelloWorld/src/source/MainFrame.cpp
+++ wxWidgets/HelloWorld/src/source/MainFrame.cpp
@@ -1,9 +1,46 @@
 #include "../header/MainFrame.hpp"
 #include "../BlueBall.XPM"
 
+BEGIN_EVENT_TABLE(MainFrame, wxFrame)
+/*OnAbout handler is bound dynamically*/
+/*Connect(wxID_ABOUT,....*/
+/**///EVT_MENU(wxID_ABOUT, MainFrame::OnAbout)
+/**/EVT_MENU(wxID_EXIT, MainFrame::OnQuit)
+END_EVENT_TABLE()
+
 MainFrame::MainFrame(const wxString& title) :
 	wxFrame(nullptr, wxID_ANY, title)
 {
+	SetProcessDPIAware();
 	SetIcon(wxIcon(main_icon_xpm));
+
+	auto fileMenu = new wxMenu();
+	fileMenu->Append(wxID_EXIT, wxT("E&xit\tAlt-X"),
+		wxT("Quit Application"));
+
+	auto helpMenu = new wxMenu();
+	helpMenu->Append(wxID_ABOUT, wxT("&About\tF1"),
+		wxT("About"));
+
+	auto menuBar = new wxMenuBar();
+	menuBar->Append(fileMenu, wxT("&File"));
+	menuBar->Append(helpMenu, wxT("&Help"));
+	SetMenuBar(menuBar);
+
+	/*It is also possible to bind handler dynamically*/
+	Connect(wxID_ABOUT, wxEVT_COMMAND_MENU_SELECTED,
+		wxCommandEventHandler(MainFrame::OnAbout));
+
 	Centre();
+}
+
+void MainFrame::OnQuit(wxCommandEvent& event)
+{
+	Close();
+}
+
+void MainFrame::OnAbout(wxCommandEvent& event)
+{
+	event.Skip();
+	wxBell();
 }
