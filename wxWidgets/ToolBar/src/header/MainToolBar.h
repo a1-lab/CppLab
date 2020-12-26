fdc1468da80c1d4c63d654a0b936c5f1f7c7d1c9
a1-lab <alphamailix@gmail.com>
2020-12-26 14:43:39 +0200

    ToolBar: add bitmaps

--- wxWidgets/ToolBar/src/header/MainToolBar.h
+++ wxWidgets/ToolBar/src/header/MainToolBar.h
@@ -1,3 +1,10 @@
 #pragma once
 #include "../bitmaps/new.xpm"
 #include "../bitmaps/open.xpm"
+#include "../bitmaps/save.xpm"
+#include "../bitmaps/copy.xpm"
+#include "../bitmaps/cut.xpm"
+#include "../bitmaps/paste.xpm"
+#include "../bitmaps/preview.xpm"
+#include "../bitmaps/print.xpm"
+#include "../bitmaps/help.xpm"
