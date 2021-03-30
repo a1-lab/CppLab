dfb0988aa119cd6a9fe50d4cfa84eaefa6cae7b0
a1-lab <alphamailix@gmail.com>
2021-03-30 18:19:36 +0300

    02_DocViewWithDocAndView: add document and view

--- wxWidgets/DocViewTutorial/02_DocViewWithDocAndView/src/source/Application.cpp
+++ wxWidgets/DocViewTutorial/02_DocViewWithDocAndView/src/source/Application.cpp
@@ -1,5 +1,7 @@
 #include "Application.hpp"
 #include "MainFrame.hpp"
+#include "Document.hpp"
+#include "View.hpp"
 //wxmemdbg.h must be the last one in include list
 #include "wxmemdbg.h"
 
@@ -11,6 +13,12 @@ bool Application::OnInit()
 
 	m_docManager = new wxDocManager();
 	m_docManager->SetMaxDocsOpen(1);
+
+	auto templ = new wxDocTemplate(m_docManager,
+		wxT("Text files"), wxT("*.txt"), wxEmptyString,
+		wxT("txt"), wxT("Document"), wxT("Template"),
+		CLASSINFO(Document),
+		CLASSINFO(View));
 
 	auto frame = new MainFrame(m_docManager, wxSize(800, 600));
 	frame->Show(true);
---
+++ wxWidgets/DocViewTutorial/02_DocViewWithDocAndView/src/source/docview/View.cpp
@@ -0,0 +1,6 @@
+#include "View.hpp"
+
+IMPLEMENT_DYNAMIC_CLASS(View, wxView)
+
+void View::OnDraw(wxDC* dc) {
+}
