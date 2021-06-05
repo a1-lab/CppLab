3cfb9e4b7b074ed83a4c414cb823057520fb377f
a1-lab <alphamailix@gmail.com>
2021-06-05 18:22:48 +0300

    MemoryLeakDetectionExample: initial commit

---
+++ MemoryLeakDetectionExample/MemoryLeakDetectionExample.cpp
@@ -0,0 +1,21 @@
+﻿/*https://docs.microsoft.com/en-us/visualstudio/debugger/finding-memory-leaks-using-the-crt-library?view=vs-2019#enable-memory-leak-detection*/
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello World!\n";
+
+	int* pi = new int;
+
+	_CrtDumpMemoryLeaks();
+}
+
+
+
+
+
