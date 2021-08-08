df9cade549eb8f8b136fee2a9794c4b7fa4a4375
a1-lab <65171376+a1-lab@users.noreply.github.com>
2021-08-08 15:44:25 +0300

    StaticLibExample: initial commit

---
+++ StaticLibExample/StaticMath/MathClient/src/source/MathClient.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "MathLibrary.h"
+
+using namespace MathLibrary;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    double a = 7.4;
+    int b = 2;
+
+    std::cout << "a+b=" << Arithmetic::Add(a, b) << std::endl;
+    std::cout << "a-b=" << Arithmetic::Subtract(a, b) << std::endl;
+}
---
+++ StaticLibExample/StaticMath/MathLibrary/src/source/MathLibrary.cpp
@@ -0,0 +1,21 @@
+﻿#include "..\header\MathLibrary.h"
+
+double MathLibrary::Arithmetic::Add(double a, double b)
+{
+	return a+b;
+}
+
+double MathLibrary::Arithmetic::Subtract(double a, double b)
+{
+	return a-b;
+}
+
+double MathLibrary::Arithmetic::Multiply(double a, double b)
+{
+	return a*b;
+}
+
+double MathLibrary::Arithmetic::Divide(double a, double b)
+{
+	return a/b;
+}
